f6e882b1eba1a4a16508106e72397068d011a498
Robin Pillar <rpillar72@gmail.com>
2022-02-18 16:52:07 +0000

    changed default input text

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -4,7 +4,7 @@
       @input="updateInstructionValue"
       class="mb-2"
       label="Tell Me What To Do"
-      initalValue="Tell me a story"
+      initalValue="Write a story about a Pokémon"
     />
     <button class="w-full px-2 py-4 bg-white" @click="generateStory">
       Generate
