7fbcb5b83ba4714449b6347b2c313a425169bc79
Robin Pillar <rpillar72@gmail.com>
2022-02-24 17:43:09 +0000

    changed hero style lang to postcss

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,13 +1,13 @@
 <template>
   <section
-    class="global-hero flex flex-col items-center justify-center bg-gradient-to-br from-green-400 to-green-600 min-h-[500px]"
+    class="hero flex flex-col items-center justify-center bg-gradient-to-br from-green-400 to-green-600 min-h-[500px]"
   >
     <h1 class="text-slate-100 text-7xl font-black mb-50">AI Text Generator</h1>
   </section>
 </template>
 
 <style lang="postcss">
-  .global-hero {
+  .hero {
     clip-path: polygon(0 0, 100% 0, 100% 75%, 50% 100%, 0 75%);
   }
 </style>
